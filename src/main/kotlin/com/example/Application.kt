fade0216daa356554f912bd360d9749e6b27fbbe
androiddevnotes <awesomedevnotes@gmail.com>
2024-10-03 17:11:41 +0100

    Enable development mode logging, update startup messages, and modify response text

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -2,11 +2,17 @@ package com.example
 
 import com.example.plugins.*
 import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
 
 fun main(args: Array<String>) {
-    io.ktor.server.netty.EngineMain.main(args)
+    EngineMain.main(args)
 }
 
 fun Application.module() {
     configureRouting()
+    
+    // You can add this line to log whether the application is running in development mode
+    log.info("Development mode: ${environment.developmentMode}")
+    log.info("Server started on port ${environment.config.port}")
 }
