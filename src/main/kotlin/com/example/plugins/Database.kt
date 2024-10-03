716f6516a7e735657282e9f69c0115445ccd1fc5
androiddevnotes <awesomedevnotes@gmail.com>
2024-10-03 20:02:25 +0100

    Comment out database configuration code in Database.kt

--- src/main/kotlin/com/example/plugins/Database.kt
+++ src/main/kotlin/com/example/plugins/Database.kt
@@ -5,18 +5,18 @@ import io.ktor.server.application.*
 import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.transactions.*
 
-fun Application.configureDatabase() {
-    val driverClassName = environment.config.property("database.driverClassName").getString()
-    val jdbcURL = environment.config.property("database.jdbcURL").getString()
-    val user = environment.config.property("database.user").getString()
-    val password = environment.config.property("database.password").getString()
-    val database = Database.connect(jdbcURL, driverClassName, user, password)
-
-    // Log the development port
-    val port = environment.config.property("ktor.deployment.port").getString()
-    log.info("Development port LOGGING: $port")
-
-    transaction(database) {
-        SchemaUtils.create(Quotes)
-    }
-}
+//fun Application.configureDatabase() {
+//    val driverClassName = environment.config.property("database.driverClassName").getString()
+//    val jdbcURL = environment.config.property("database.jdbcURL").getString()
+//    val user = environment.config.property("database.user").getString()
+//    val password = environment.config.property("database.password").getString()
+//    val database = Database.connect(jdbcURL, driverClassName, user, password)
+//
+//    // Log the development port
+//    val port = environment.config.property("ktor.deployment.port").getString()
+//    log.info("Development port LOGGING: $port")
+//
+//    transaction(database) {
+//        SchemaUtils.create(Quotes)
+//    }
+//}
