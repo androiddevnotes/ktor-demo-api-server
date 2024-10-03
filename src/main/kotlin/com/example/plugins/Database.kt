1af7940c255857a0cfbc85e150f6ced771fad3d2
androiddevnotes <awesomedevnotes@gmail.com>
2024-10-03 19:01:30 +0100

    Log the development port during database configuration in development environment

--- src/main/kotlin/com/example/plugins/Database.kt
+++ src/main/kotlin/com/example/plugins/Database.kt
@@ -12,6 +12,10 @@ fun Application.configureDatabase() {
     val user = environment.config.property("database.user").getString()
     val password = environment.config.property("database.password").getString()
     val database = Database.connect(jdbcURL, driverClassName, user, password)
+
+    // Log the development port
+    val port = environment.config.property("ktor.deployment.port").getString()
+    log.info("Development port LOGGING: $port")
     
     transaction(database) {
         SchemaUtils.create(Quotes)
